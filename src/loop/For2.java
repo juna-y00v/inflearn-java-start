fc6852683e10d86a01a00e0140528844cc48ebef
Yjunha <junaY00v@gmail.com>
2024-06-17 23:35:48 +0900

    Section 5. 반복문 - for문1

--- src/loop/For2.java
+++ src/loop/For2.java
@@ -10,7 +10,7 @@ public class For2 {
 
         for (int i = 1; i <= endNum; i++) {
             sum = sum + i;
-            System.out.println("i = " + i+" sum = " + sum);
+            System.out.println("i = " + i  + " sum = " + sum);
         }
     }
 }
