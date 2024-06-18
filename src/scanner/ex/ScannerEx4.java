0cb714391d0ea2e5a95badf5399101a8f890ebd0
Yjunha <junaY00v@gmail.com>
2024-06-18 16:10:23 +0900

    Section 7. 훈련 - 문제와 풀이1

---
+++ src/scanner/ex/ScannerEx4.java
@@ -0,0 +1,18 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerEx4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("구구단의 단 수를 입력해주세요:");
+        int number = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + number * i);
+        }
+
+    }
+}
